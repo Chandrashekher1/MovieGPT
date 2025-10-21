1724cde5fa4fbff670940f3916c8b3e39ce17e90
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-10-21 14:36:06 +0530

    style: adjust margin and padding of buttons

--- src/Components/VideoTitle.jsx
+++ src/Components/VideoTitle.jsx
@@ -4,17 +4,16 @@ import { useSelector } from 'react-redux';
 const VideoTitle = ({title,overview}) => {
 
   const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-
   const addNowPlayingMovies = useSelector((store) => store.movies)
   const movieId = addNowPlayingMovies.nowPlayingMovies[0]?.id
   
   return (
-    <div className=' w-screen aspect-video pt-[35%]  md:pt-[15%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black'>
+    <div className=' w-screen aspect-video pt-[28%]  md:pt-[15%] px-6 md:px-16 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-2xl md:text-6xl font-bold'>{title}</h1>
-        <h1 className=' hidden md:inline-block p-6 text-lg my-3 w-1/4 '>{overview}</h1>
+        <span className=' hidden md:inline-block py-4 md:text-lg mb-4 w-1/2 '>{overview}</span>
         <div className='my-2 md:-mt-8'>
-            <a href={`https://www.youtube.com/watch?v=${trailerVideo?.key}`}><button className='bg-white text-black p-1  md:inline-block text-xl px-12 p-3 rounded-lg hover:opacity-80 cursor-pointer' >▶  Play</button></a>
-            <a href={`https://www.themoviedb.org/movie/${movieId}`}><button  className=' hidden md:inline-block bg-gray-500  text-white m-3 text-xl px-10 p-3 rounded-lg bg-opacity-50 cursor-pointer'>More info</button></a>
+            <a href={`https://www.youtube.com/watch?v=${trailerVideo?.key}`}><button className='bg-white text-black py-1 px-2 md:inline-block md:text-xl md:px-12 rounded-md md:rounded-lg hover:opacity-80 cursor-pointer' >▶  Play</button></a>
+            <a href={`https://www.themoviedb.org/movie/${movieId}`}><button  className='bg-gray-500  text-white m-3 md:text-xl md:px-10 md:p-3 py-1 px-2 rounded-lg bg-opacity-50 cursor-pointer'>More info</button></a>
         </div>
     </div>
   )
