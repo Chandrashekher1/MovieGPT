b8cd72b13976e0fe5bd8660e1517e6a8ecfd1b7e
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-10-21 15:16:02 +0530

    feat: add overview of each movie

--- src/Components/GptMovieList.jsx
+++ src/Components/GptMovieList.jsx
@@ -7,17 +7,18 @@ const GptMovieList = ({ movie }) => {
     }
   
     return (
-      <div className="p-6 bg-black flex my-8 ">
+      <div className="px-1 md:px-6 py-2 bg-black flex my-4 ">
         <div className="m-2">
           <MovieCard
             key={movie.id}
             posterPath={movie.poster_path}
+            overview={movie.overview}
           />
         </div>
         <div>
             <h1 className="text-lg md:text-3xl my-4 font-bold py-4 text-white">{movie.title}</h1>
             <h1 className="text-lg md:text-2xl my-2 text-gray-400">{movie.release_date}</h1>
-            <h1 className="text-lg hidden md:inline-block text-xl text-white">{movie.overview}</h1>
+            <h1 className="hidden md:inline-block md:text-xl text-white">{movie.overview}</h1>
         </div>
        
       </div>
